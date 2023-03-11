c9f5077265d968fc9d597afd920a98ca35cb6cca
globalsetfix <globalsetfix@gmail.com>
2023-03-12 00:20:21 +0200

    Strings

---
+++ basics_4.py
@@ -0,0 +1,23 @@
+#14 Strings
+x = "hello" + " " \
+    + " programmer"
+y = 'I\'m progger'
+print(y)
+
+z = "Hello \n YouRa"
+z = "Hello \n     \rYouRa"
+print(z)
+
+m = "12\t34567"
+print(m)
+
+t = "\t Hello! \n\tI'm very glad to see you!"
+print(t)
+
+s = """This is text with "triple code" """
+print(s)
+
+s = """This is text 
+with "triple code" """
+
+print(s)
