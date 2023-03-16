50f66a5b00c8d8d2c5fc911dae82582f1b640f74
globalsetfix <globalsetfix@gmail.com>
2023-03-16 23:57:17 +0200

    #15 Indexing & Slicing

--- basics_4.py
+++ basics_4.py
@@ -21,3 +21,35 @@ s = """This is text
 with "triple code" """
 
 print(s)
+
+#15 Indexing & Slicing
+
+greeting = "Hello python!"
+greeting_len = len(greeting)
+#Length
+print(greeting_len)
+#First
+print(greeting[0])
+#Last
+print(greeting[-1])
+
+#Slicing ( кусочки )
+#from - to
+print(greeting[1:5])
+print(greeting[-5:-2])
+
+print(greeting[2:])
+print(greeting[:2])
+
+print(greeting[:])
+
+#Step
+print(greeting[::2])
+print(greeting[1::3])
+print(greeting[1:9:2])
+
+#revers
+print(greeting[::-1])
+
+
+
